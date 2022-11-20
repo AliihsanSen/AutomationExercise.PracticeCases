bd40dbff5b32bb38b171073cacdaebdf2bdd6c0f
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-20 13:17:16 +0300

    20.11.2022

--- src/test/java/AutomationExercise/Q22_AddToCartFromRecommendedItems.java
+++ src/test/java/AutomationExercise/Q22_AddToCartFromRecommendedItems.java
@@ -1,4 +1,37 @@
 package AutomationExercise;
 
-public class Q22_AddToCartFromRecommendedItems {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class Q22_AddToCartFromRecommendedItems extends TestBase {
+
+    @Test
+    public void name() throws InterruptedException {
+
+        // 1. Launch browser
+        // 2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        // 3. Scroll to bottom of page
+        WebElement allRightsText=driver.findElement(By.xpath("//*[text()='Copyright © 2021 All rights reserved']"));
+        actions.moveToElement(allRightsText).perform();
+
+        // 4. Verify 'RECOMMENDED ITEMS' are visible
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='recommended items']")).isDisplayed());
+
+        // 5. Click on 'Add To Cart' on Recommended product
+        actions.sendKeys(Keys.PAGE_UP).perform();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("(//a[text()='Add to cart'])[69]")).click();
+
+        // 6. Click on 'View Cart' button
+        driver.findElement(By.xpath("//*[text()='View Cart']")).click();
+
+        // 7. Verify that product is displayed in cart page
+        Assert.assertTrue(driver.findElement(By.xpath("//tr[@id='product-1']")).isDisplayed());
+    }
 }
