1ce196f12cb89d48bd81319ca99598f6e73f0df9
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-20 12:37:16 +0300

    20.11.2022

--- src/test/java/AutomationExercise/Q09_SearchProduct.java
+++ src/test/java/AutomationExercise/Q09_SearchProduct.java
@@ -1,4 +1,41 @@
 package AutomationExercise;
 
-public class Q09_SearchProduct {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Q09_SearchProduct extends TestBase {
+
+    @Test
+    public void name() throws InterruptedException {
+
+        // 1. Launch browser
+        // 2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        // 3. Verify that home page is visible successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()=' Home']")).isDisplayed());
+
+        // 4. Click on 'Products' button
+        driver.findElement(By.xpath("//*[text()=' Products']")).click();
+
+        // 5. Verify user is navigated to ALL PRODUCTS page successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//h2[@class='title text-center']")).isDisplayed());
+
+        // 6. Enter product name in search input and click search button
+        driver.findElement(By.xpath("//input[@id='search_product']")).sendKeys("women");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@id='submit_search']")).click();
+
+        // 7. Verify 'SEARCHED PRODUCTS' is visible
+        Assert.assertTrue(driver.findElement(By.xpath("//h2[@class='title text-center']")).isDisplayed());
+
+        // 8. Verify all the products related to search are visible
+        List<WebElement> list=driver.findElements(By.xpath("//div[@class='product-overlay']"));
+        Assert.assertFalse(list.isEmpty());
+    }
 }
--- src/test/java/AutomationExercise/Q10_VerifySubscriptionInHomePage.java
+++ src/test/java/AutomationExercise/Q10_VerifySubscriptionInHomePage.java
@@ -1,4 +1,36 @@
 package AutomationExercise;
 
-public class Q10_VerifySubscriptionInHomePage {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Q10_VerifySubscriptionInHomePage extends TestBase {
+
+    @Test
+    public void name() {
+
+        // 1. Launch browser
+        // 2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        // 3. Verify that home page is visible successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()=' Home']")).isDisplayed());
+
+        // 4. Scroll down to footer
+        WebElement footer=driver.findElement(By.xpath("//footer[@id='footer']"));
+        actions.moveToElement(footer).perform();
+
+        // 5. Verify text 'SUBSCRIPTION'
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Subscription']")).isDisplayed());
+
+        // 6. Enter email address in input and click arrow button
+        driver.findElement(By.xpath("//input[@id='susbscribe_email']")).sendKeys(faker.internet().emailAddress());
+        driver.findElement(By.xpath("//button[@id='subscribe']")).click();
+
+        // 7. Verify success message 'You have been successfully subscribed!' is visible
+        Assert.assertTrue(driver.findElement(By.xpath("//div[text()='You have been successfully subscribed!']")).isDisplayed());
+
+    }
 }
