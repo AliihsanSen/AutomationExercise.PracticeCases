14becc0958613906ccaac5c040aa2c2c3c657b67
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-20 12:40:43 +0300

    20.11.2022

--- src/test/java/AutomationExercise/Q07_VerifyTestCasesPage.java
+++ src/test/java/AutomationExercise/Q07_VerifyTestCasesPage.java
@@ -1,4 +1,27 @@
 package AutomationExercise;
 
-public class Q07_VerifyTestCasesPage {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class Q07_VerifyTestCasesPage extends TestBase {
+
+    @Test
+    public void name() {
+
+        // 1. Launch browser
+        // 2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        // 3. Verify that home page is visible successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()=' Home']")).isDisplayed());
+
+        // 4. Click on 'Test Cases' button
+        driver.findElement(By.xpath("(//button[@class='btn btn-success'])[1]")).click();
+
+        // 5. Verify user is navigated to test cases page successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Test Cases']")).isDisplayed());
+
+    }
 }
